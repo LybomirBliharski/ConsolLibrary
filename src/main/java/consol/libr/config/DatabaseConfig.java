4b1bcf06f6f8464b15ca65e1311128d71a489fb7
LybomirBliharski <lybomir.bliharski@gmail.com>
2017-02-18 13:12:16 +0200

    add db properties and code cleaning

--- src/main/java/consol/libr/config/DatabaseConfig.java
+++ src/main/java/consol/libr/config/DatabaseConfig.java
@@ -6,7 +6,10 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -20,15 +23,25 @@ import consol.libr.dao.impl.BookDaoImpl;
 
 @Configuration
 @EnableTransactionManagement
+@PropertySources({ @PropertySource("consol/libr/config/application.properties"), })
 public class DatabaseConfig {
+
+	@Autowired
+	private Environment env;
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private LocalContainerEntityManagerFactoryBean entityManagerFactory;
 
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost/lib?useSSL=false");
-		dataSource.setUsername("root");
-		dataSource.setPassword("\u0444\u0432\u044C\u0448\u0442");
+		dataSource.setDriverClassName(env.getProperty("db.driver"));
+		dataSource.setUrl(env.getProperty("db.url"));
+		dataSource.setUsername(env.getProperty("db.username"));
+		dataSource.setPassword(env.getProperty("db.password"));
 		return dataSource;
 	}
 
@@ -39,7 +52,7 @@ public class DatabaseConfig {
 		entityManagerFactory.setDataSource(dataSource);
 
 		// Classpath scanning of @Component, @Service, etc annotated class
-		entityManagerFactory.setPackagesToScan("consol.libr.*");
+		entityManagerFactory.setPackagesToScan("consol.libr*");
 
 		// Vendor adapter
 		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
@@ -47,9 +60,9 @@ public class DatabaseConfig {
 
 		// Hibernate properties
 		Properties additionalProperties = new Properties();
-		additionalProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-		additionalProperties.put("hibernate.show_sql", "false");
-		additionalProperties.put("hibernate.hbm2ddl.auto", "update");
+		additionalProperties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+		additionalProperties.put("hibernate.show_sql", false);
+		additionalProperties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
 		entityManagerFactory.setJpaProperties(additionalProperties);
 
 		return entityManagerFactory;
@@ -71,14 +84,4 @@ public class DatabaseConfig {
 	public BookDao bookdao() {
 		return new BookDaoImpl();
 	}
-
-	@Autowired
-	private Environment env;
-
-	@Autowired
-	private DataSource dataSource;
-
-	@Autowired
-	private LocalContainerEntityManagerFactoryBean entityManagerFactory;
-
 }
--- src/main/java/consol/libr/dao/impl/BookDaoImpl.java
+++ src/main/java/consol/libr/dao/impl/BookDaoImpl.java
@@ -5,40 +5,41 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import javax.persistence.EntityManagerFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import consol.libr.dao.BookDao;
 import consol.libr.entity.Book;
 
 @Repository
-@Transactional
 public class BookDaoImpl implements BookDao {
 	@PersistenceContext
 	private EntityManager entityManager;
 
-	
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public void add(Book book) {
 		entityManager.persist(book);
-		
-	}
-	
-	public List<Book> findAll() {
-		List<Book> list = entityManager.createNativeQuery("SELECT * FROM books", Book.class).getResultList();
-        return list;
 
 	}
-	
-	public List<Book> findAllbyName(String name) {
-return entityManager.createQuery("SELECT book FROM Book book WHERE book.name = :name", Book.class)
-				.setParameter("name", name).getResultList();
+
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+	public List<Book> findAll() {
+		return entityManager.createNativeQuery("SELECT * FROM books ORDER BY book_name ASC", Book.class).getResultList();
+
 	}
-	
+
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+	public List<Book> findAllbyName(String name) {
+		return entityManager.createQuery("SELECT book FROM Book book WHERE book.name = :name", Book.class).setParameter("name", name).getResultList();
+	}
+
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public void delete(Book book) {
 		entityManager.remove(entityManager.merge(book));
 	}
-	
+
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public Book update(Book book) {
 		return entityManager.merge(book);
 	}
